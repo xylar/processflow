aa1f41df991444eaa8bde4cbf8147853a2b63556
Sterling Baldwin <baldwin32@llnl.gov>
2018-08-01 17:19:26 -0700

    Fleshes out pbs interface

--- lib/pbs.py
+++ lib/pbs.py
@@ -1,7 +1,7 @@
 import os
 import logging
-from time import sleep
 from subprocess import Popen, PIPE
+from jobinfo import JobInfo
 
 
 class PBS(object):
@@ -21,20 +21,84 @@ class PBS(object):
         """
         Submit to the batch queue in non-interactive mode
 
-        Parameters:
+        Parameters
+        ----------
             cmd (str): The path to the run script that should be submitted
-            sargs (str): The additional arguments to pass to pbs
-        Returns:
+            pargs (str): The additional arguments to pass to pbs
+        Returns
+        -------
             job id of the new job (int)
         """
-        pass
+        command = ['qsub', cmd, pargs] if pargs is not None else ['qsub', cmd]
+        proc = Popen(command, shell=False, stderr=PIPE, stdout=PIPE)
+        out, err = proc.communicate()
+        if err:
+            logging.error(err)
+        else:
+            return out.rstrip()
     
     def showjob(self, jobid):
         """
         A wrapper around 'qstat -f JOBID' to get job info
 
-        Parameters:
+        Parameters
+        ----------
             jobid (str): the job id to get information about
-        Returns:
-            A dictionary of information provided by PBS about the job
+        Returns
+        -------
+            A jobinfo object holding information about the job
         """
+        cmd = ['qstat', '-f', jobid]
+        proc = Popen(cmd, shell=False, stderr=PIPE, stdout=PIPE)
+        out, err = proc.communicate()
+        if err:
+            logging.error(err)
+        job_info = JobInfo()
+        for idx, item in enumerate(out.split('\n')):
+            if idx == 0:
+                index = item.find(':')
+            else:
+                index = item.find('=')
+                if index <= 0:
+                    continue
+            attr = item[:index].strip()
+            attr = self.pbs_to_jobinfo(attr)
+            if attr is None:
+                continue
+            job_info.set_attr(
+                attr=attr.strip(),
+                val=item[index + 1:].strip())
+        return job_info
+    
+    def pbs_to_jobinfo(self, attr):
+        if attr == 'queue':
+            return 'PARTITION'
+        elif attr == 'Job_Owner':
+            return 'USER'
+        elif attr == 'Job_Name':
+            return 'NAME'
+        elif attr == 'job_state':
+            return 'STATE'
+        elif attr == 'Job Id':
+            return 'JOBID'
+        else:
+            return None
+
+
+    def get_node_number(self, queue='acme'):
+        """
+        Return the number of nodes available in the requested queue
+
+        Parameters
+        ----------
+            queue (str): the queue to check, defaults to 'acme'
+        Returns
+        -------
+            (int): the number of nodes available to the resource manager
+        """
+        cmd = ['pbsnodes -a | grep "properties = {}" | wc -l'.format(queue)]
+        proc = Popen(cmd, shell=False, stderr=PIPE, stdout=PIPE)
+        out, err = proc.communicate()
+        if err:
+            logging.error(err)
+        return int(out.strip())
