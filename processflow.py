e3825a3f9bd1d8c5505ca1d8564ac1cdc2936bed
Sterling Baldwin <baldwin32@llnl.gov>
2018-08-02 09:44:59 -0700

    experimental build

--- processflow.py
+++ processflow.py
@@ -20,8 +20,8 @@ from lib.util import print_line
 from lib.util import print_message
 from lib.util import print_debug
 
-__version__ = '2.0.3'
-__branch__ = 'nightly'
+__version__ = '2.0.4'
+__branch__ = 'experimental'
 
 os.environ['UVCDAT_ANONYMOUS_LOG'] = 'False'
 os.environ['NCO_PATH_OVERRIDE'] = 'True'
