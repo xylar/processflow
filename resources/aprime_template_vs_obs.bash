6e64bee910e92058a8cda24170dec04dadb69569
Sterling Baldwin <baldwin32@llnl.gov>
2018-10-18 09:52:13 -0700

    Updates the e3sm-unified versions in the env_loader as well as the aprime run script

--- resources/aprime_template_vs_obs.bash
+++ resources/aprime_template_vs_obs.bash
@@ -378,42 +378,41 @@ fi
 
 # LOAD THE MACHINE-SPECIFIC ANACONDA-2.7 ENVIRONMENT
 source $MODULESHOME/init/bash  
-if [ $machname == "edison" ]; then
+if [ ${HOSTNAME:0:3} == "nid" ]; then
   module unload python
   module unload python_base
   source /global/project/projectdirs/acme/software/anaconda_envs/edison/base/etc/profile.d/conda.sh
-  conda activate e3sm_unified_1.2.0_py2.7_nox
+  conda activate e3sm_unified_1.2.1_py2.7_nox
   export NCO_PATH_OVERRIDE=No
-elif [ $machname == "cori" ]; then
+elif [ ${HOSTNAME:0:4} == "cori" ]; then
   module unload python
   module unload python_base
-  source /global/project/projectdirs/acme/software/anaconda_envs/cori/base/etc/profile.d/conda.sh
-  conda activate e3sm_unified_1.2.0_py2.7_nox
+  module use global/project/projectdirs/acme/software/modulefiles/all
+  module load e3sm-unified/1.2.3
   export NCO_PATH_OVERRIDE=No
-elif [ $machname == "rhea" ] || [ $machname == "titan" ]; then
+elif [ ${HOSTNAME:0:4} == "rhea" ] || [ ${HOSTNAME:0:5} == "titan" ]; then
   module unload python
-  source /ccs/proj/cli900/sw/rhea/e3sm-unified/base/etc/profile.d/conda.sh
-  conda activate e3sm_unified_1.2.0_py2.7_nox
+  module use /ccs/proj/cli900/sw/rhea/modulefiles/all
+  module load e3sm-unified/1.2.3
   export NCO_PATH_OVERRIDE=No
-elif [ $machname == "acme1" ]; then
-  source /usr/local/e3sm_unified/envs/base/etc/profile.d/conda.sh
-  conda activate e3sm_unified_1.2.0_py2.7_nox
+elif [ ${HOSTNAME:0:5} == "aims4" ] || [ ${HOSTNAME:0:5} == "acme1" ]; then
+  module use /usr/local/e3sm_unified/modulefiles
+  module load e3sm-unified/1.2.3
   export NCO_PATH_OVERRIDE=No
-elif [ $machname == "aims4" ]; then
-  source /usr/local/e3sm_unified/envs/base/etc/profile.d/conda.sh
-  conda activate e3sm_unified_1.2.0_py2.7_nox
-  export NCO_PATH_OVERRIDE=No
-elif [ $machname == "lanl" ]; then
+elif [ ${HOSTNAME:0:4} == "wolf" ] || [ ${HOSTNAME:0:7} == "grizzly" ]; then
   module unload python
   source /usr/projects/climate/SHARED_CLIMATE/anaconda_envs/base/etc/profile.d/conda.sh
-  conda activate e3sm_unified_1.2.0_py2.7_nox
-elif [ $machname == "anvil" ]; then
+  conda activate e3sm_unified_1.2.1_py2.7_nox
+elif [ ${HOSTNAME:0:6} == "blogin" ] || ([ ${HOSTNAME:0:1} == "b" ] && [[ ${HOSTNAME:1:2} =~ [0-9] ]]); then
   source /lcrc/soft/climate/e3sm-unified/base/etc/profile.d/conda.sh
-  conda activate e3sm_unified_1.2.0_py2.7_nox
+  conda activate e3sm_unified_1.2.1_py2.7_nox
   unset LD_LIBRARY_PATH
-elif [ $machname == "cooley" ]; then
-  source /lus/theta-fs0/projects/ClimateEnergy_2/software/e3sm_unified/base/etc/profile.d/conda.sh
-  conda activate e3sm_unified_1.2.0_py2.7_nox
+elif [ ${HOSTNAME:0:6} == "cooley" ] || ([ ${HOSTNAME:0:2} == "cc" ] && [[ ${HOSTNAME:2:3} =~ [0-9] ]]); then
+  module use /lus/theta-fs0/projects/ccsm/acme/tools/modulefiles
+  module load e3sm-unified/1.2.3
+else
+  echo "Running on an unsupported machine. Sourcing users anaconda environment from {{ user_env_path }}"
+  source activate {{ user_env_path }}
 fi
 
 # The following is needed to avoid the too-many-open-files problem
--- resources/env_loader.bash
+++ resources/env_loader.bash
@@ -9,17 +9,17 @@ if [ ${HOSTNAME:0:3} == "nid" ]; then
 elif [ ${HOSTNAME:0:4} == "cori" ]; then
   module unload python
   module unload python_base
-  source /global/project/projectdirs/acme/software/anaconda_envs/cori/base/etc/profile.d/conda.sh
-  conda activate e3sm_unified_1.2.1_py2.7_nox
+  module use global/project/projectdirs/acme/software/modulefiles/all
+  module load e3sm-unified/1.2.3
   export NCO_PATH_OVERRIDE=No
 elif [ ${HOSTNAME:0:4} == "rhea" ] || [ ${HOSTNAME:0:5} == "titan" ]; then
   module unload python
-  source /ccs/proj/cli900/sw/rhea/e3sm-unified/base/etc/profile.d/conda.sh
-  conda activate e3sm_unified_1.2.1_py2.7_nox
+  module use /ccs/proj/cli900/sw/rhea/modulefiles/all
+  module load e3sm-unified/1.2.3
   export NCO_PATH_OVERRIDE=No
 elif [ ${HOSTNAME:0:5} == "aims4" ] || [ ${HOSTNAME:0:5} == "acme1" ]; then
-  source /usr/local/e3sm_unified/envs/base/etc/profile.d/conda.sh
-  conda activate e3sm_unified_1.2.1_py2.7_nox
+  module use /usr/local/e3sm_unified/modulefiles
+  module load e3sm-unified/1.2.3
   export NCO_PATH_OVERRIDE=No
 elif [ ${HOSTNAME:0:4} == "wolf" ] || [ ${HOSTNAME:0:7} == "grizzly" ]; then
   module unload python
@@ -30,8 +30,8 @@ elif [ ${HOSTNAME:0:6} == "blogin" ] || ([ ${HOSTNAME:0:1} == "b" ] && [[ ${HOST
   conda activate e3sm_unified_1.2.1_py2.7_nox
   unset LD_LIBRARY_PATH
 elif [ ${HOSTNAME:0:6} == "cooley" ] || ([ ${HOSTNAME:0:2} == "cc" ] && [[ ${HOSTNAME:2:3} =~ [0-9] ]]); then
-  source /lus/theta-fs0/projects/ClimateEnergy_2/software/e3sm_unified/base/etc/profile.d/conda.sh
-  conda activate e3sm_unified_1.2.1_py2.7_nox
+  module use /lus/theta-fs0/projects/ccsm/acme/tools/modulefiles
+  module load e3sm-unified/1.2.3
 else
   echo "Running on an unsupported machine. Sourcing users anaconda environment from {{ user_env_path }}"
   source activate {{ user_env_path }}
