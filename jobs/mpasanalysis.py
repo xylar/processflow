f3a3f2bd2efe18de7a366c41511b77228a68ba98
Baldwin <baldwin32@ml-9635195.llnl.gov>
2018-12-05 10:35:23 -0800

    MPAS-Analysis is running correctly

--- jobs/mpasanalysis.py
+++ jobs/mpasanalysis.py
@@ -3,6 +3,7 @@ import re
 import logging
 
 from shutil import move
+from subprocess import Popen, PIPE
 
 from jobs.diag import Diag
 from lib.util import render, print_line
@@ -36,6 +37,17 @@ class MPASAnalysis(Diag):
             self._short_comp_name = 'obs'
         else:
             self._short_comp_name = kwargs['config']['simulations'][self.comparison]['short_name']
+        
+        # sets up the output path, creating it if it doesnt already exist
+        self._output_path = os.path.join(
+            kwargs['config']['global']['project_path'],
+            'output', 'diags', self.short_name, 'mpas_analysis',
+            '{start:04d}_{end:04d}_vs_{comp}'.format(
+                start=self.start_year,
+                end=self.end_year,
+                comp=self._short_comp_name))
+        if not os.path.exists(self._output_path):
+            os.makedirs(self._output_path)
     # -----------------------------------------------
     def setup_dependencies(self, *args, **kwargs):
         """
@@ -55,23 +67,17 @@ class MPASAnalysis(Diag):
                 and the scripts generated, but not actually submitted
         """
         self._dryrun = dryrun
-
-        # sets up the output path, creating it if it doesnt already exist
-        self._output_path = os.path.join(
-            config['global']['project_path'],
-            'output', 'diags', self.short_name, 'mpas_analysis',
-            '{start:04d}_{end:04d}_vs_{comp}'.format(
-                start=self.start_year,
-                end=self.end_year,
-                comp=self._short_comp_name))
-        if not os.path.exists(self._output_path):
-            os.makedirs(self._output_path)
+        mpas_config = config['diags']['mpas_analysis']
 
         if config.get('img_hosting'):
             self._host_path = os.path.join(
                 config['img_hosting']['host_directory'],
                 self.short_name,
-                self._job_type)
+                self._job_type, 
+                '{:04d}_{:04d}_vs_{}'.format(
+                    self._start_year,
+                    self._end_year,
+                    self._short_comp_name))
         else:
             self._host_path = 'html'
 
@@ -87,33 +93,41 @@ class MPASAnalysis(Diag):
         template_input_path = os.path.join(
             config['global']['resource_path'],
             'mpas_a_vs_obs.cfg')
+
+        # create the "generate_plots" string
+        generate_string = '["' + '", "'.join([x for x in mpas_config.get('generate_plots', '')]) + '"]'
         
-        generate_string = '["' + '", "'.join([x for x in config['diags']['mpas_analysis'].get('generate_plots', '')]) + '"]'
+        # setup the start year offset for nino and ts plots
+        if self._start_year == int(config['simulations']['start_year']):
+            start_offset = 0
+        else:
+            start_offset = int(mpas_config.get('start_year_offset', 0))
+
         variables = {
             'case': self.case,
-            'numWorkers': config['diags']['mpas_analysis'].get('num_workers', 8),
+            'numWorkers': mpas_config.get('num_workers', 8),
             'baseInputPath': self._input_base_path,
             'restartSubPath': self._input_base_path,
             'ocnHistSubPath': self._input_base_path,
             'iceHistSubPath': self._input_base_path,
             'meshName': config['simulations'][self.case].get('native_mpas_grid_name', ''),
-            'mappingDirectory': config['diags']['mpas_analysis'].get('mapping_directory', ''),
-            'ocnNamePath': self._input_base_path,
-            'ocnStreamsPath': self._input_base_path,
-            'iceNamePath': self._input_base_path,
-            'iceStreamsPath': self._input_base_path,
+            'mappingPath': mpas_config.get('mapping_directory', ''),
+            'ocnNamelistName': mpas_config.get('ocean_namelist_name', 'mpaso_in'),
+            'ocnStreamsName': 'streams.ocean',
+            'iceNamelistName': mpas_config.get('seaice_namelist_name', 'mpassi_in'),
+            'iceStreamsName': 'streams.seaice',
             'outputBasePath': self._output_path,
             'generatePlots': generate_string,
             'startYear': self.start_year,
             'endYear': self.end_year,
-            'tsYear': config['diags']['mpas_analysis'].get('start_year_offset', self.start_year),
+            'tsStart': self.start_year - start_offset,
             'tsEnd': self.end_year,
-            'ninoStart': config['diags']['mpas_analysis'].get('start_year_offset', self.start_year),
+            'ninoStart': self.start_year - start_offset,
             'ninoEnd': self.end_year,
-            'ocnObsPath': config['diags']['mpas_analysis'].get('ocn_obs_data_path', ''),
-            'iceObs': config['diags']['mpas_analysis'].get('seaice_obs_data_path', ''),
-            'regionMaskPath': config['diags']['mpas_analysis'].get('region_mask_path', ''),
-            'runMOC': config['diags']['mpas_analysis'].get('run_MOC', ''),
+            'ocnObsPath': mpas_config.get('ocn_obs_data_path', ''),
+            'iceObs': mpas_config.get('seaice_obs_data_path', ''),
+            'regionMaskPath': mpas_config.get('region_mask_path', ''),
+            'runMOC': mpas_config.get('run_MOC', ''),
             'htmlSubdirectory': self._host_path
         }
         render(
@@ -137,17 +151,49 @@ class MPASAnalysis(Diag):
             True if all output exists as expected
             False otherwise
         """
-        return self.status == JobStatus.COMPLETED
+        # if the job ran through slurm can came back complete, just return True
+        if self.status == JobStatus.COMPLETED:
+            return True
+        elif self.status == JobStatus.FAILED:
+            return False
+        # otherwise, maybe this job hasnt been run yet in this instance, but the output might be there
+        else:
+            log_path = os.path.join(self._output_path, 'logs')
+            if os.path.exists(log_path):
+                # Check that there are actually files inside the log directory
+                if len(os.listdir(log_path)) < 50:
+                    return False
+                # grep the logs directory for any errors, if the logs directory exists, and
+                # doesnt contain any errors, then the job was probably run previously and finished successfully
+                cmd = 'grep Error {}/*.log'.format(log_path)
+                out, err = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True).communicate()
+                if out or err:
+                    return False
+                else:
+                    return True
+            # if there's no logs directory, the job has never been started
+            else:
+                return False
     # -----------------------------------------------
 
     def handle_completion(self, filemanager, event_list, config, *args, **kwargs):
         """
         Setup for webhosting after a successful run
+
+        MPAS-Analysis handles moving the files to the correct location, so no extra handling is required
         
         Parameters
         ----------
             event_list (EventList): an event list to push user notifications into
             config (dict): the global config object
         """
-        pass
+
+        if config['global']['host']:
+            self._host_url = 'https://{server}/{prefix}/{case}/mpas_analysis/{start:04d}_{end:04d}_vs_{comp}/index.html'.format(
+                server=config['img_hosting']['img_host_server'],
+                prefix=config['img_hosting']['url_prefix'],
+                case=self.short_name,
+                start=self.start_year,
+                end=self.end_year,
+                comp=self._short_comp_name)
     # -----------------------------------------------
