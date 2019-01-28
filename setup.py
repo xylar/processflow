bc06f686292abc56c2530d4bf89b3209b22a231c
Baldwin <baldwin32@ml-9635195.llnl.gov>
2019-01-28 10:09:59 -0800

    Updates version information for nightly

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ data_files = [(sys.prefix + '/share/processflow/resources',
 
 setup(
     name="e3sm_processflow",
-    version="2.1.1",
+    version="2.2.0",
     author="Sterling Baldwin",
     author_email="baldwin32@llnl.gov",
     description="E3SM Automated workflow for handling post processing and diagnostic jobs for raw model data",
