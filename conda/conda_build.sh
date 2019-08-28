716bda88e2f21d8bafdb3654b268e0961d54b276
Sterling Baldwin <baldwin32@llnl.gov>
2019-08-28 15:17:41 -0700

    trying to get the build to work again

--- conda/conda_build.sh
+++ conda/conda_build.sh
@@ -3,7 +3,7 @@ export VERSION="2.2.3"
 export BUILD_NAME="3"
 export CONDA_BLD_PATH=~/conda-bld
 USER="e3sm"
-PLATFORM="noarch"
+PLATFORM="linux-64"
 PKG="processflow"
 
 if [ -d $CONDA_BLD_PATH ]; then
