f7dcbd033dbcac918f346969e62f68fab501951f
Sterling Baldwin <baldwin32@llnl.gov>
2019-08-29 09:06:33 -0700

    Adds the missing packages

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email="baldwin32@llnl.gov",
     description="E3SM Automated workflow for handling post processing and " 
                 "diagnostic jobs for raw model data",
-    packages=["processflow"],
+    packages=['processflow', 'processflow.jobs', 'processflow.lib'],
     package_dir={'processflow': 'processflow'},
     include_package_data=True,
     entry_points={'console_scripts': ['processflow = processflow.__main__:main']})
